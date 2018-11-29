04b872b08d6e90dd3073650047630bfca8f0464c
db394217 <db394217@students.mimuw.edu.pl>
2018-11-29 20:16:41 +0100

    imperialfleet

--- src/imperialfleet.h
+++ src/imperialfleet.h
@@ -1,8 +1,60 @@
-//
-// Created by andrzej on 28.11.18.
-//
-
 #ifndef STARWARS_IMPERIALFLEET_H
 #define STARWARS_IMPERIALFLEET_H
+
+#include "rebelfleet.h"
+
+namespace {
+    enum ShipType {
+        DEATH_STAR = 0,
+        IMPERIAL_DESTROYER = 1,
+        TIE_FIGHTER = 2
+    };
+}
+
+template <typename U, ShipType T>
+class ImperialStarship {
+public:
+    using valueType = U;
+
+    ImperialStarship(U shield, const U attackPower) : shield(shield), attackPower(attackPower) {}
+
+    U getShield() {
+        return shield;
+    }
+
+    U getAttackPower() {
+        return attackPower;
+    }
+
+    void takeDamage(U damage) {
+        if (damage > shield) {
+            shield = 0;
+        } else {
+            shield -= damage;
+        }
+    }
+private:
+    U shield;
+    const U attackPower;
+};
+
+template <typename U> using DeathStar = ImperialStarship<U, DEATH_STAR>;
+template <typename U> using ImperialDestroyer = ImperialStarship<U, IMPERIAL_DESTROYER>;
+template <typename U> using TIEFighter = ImperialStarship<U, TIE_FIGHTER>;
+
+
+template <typename I, typename R> void attack(I& imperialShip, R& rebelShip) {
+    rebelShip.takeDamage(imperialShip.getAttackPower());
+}
+
+template <typename I, typename T> void attack(I& imperialShip, StarCruiser<T>& rebelShip) {
+    rebelShip.takeDamage(imperialShip.getAttackPower());
+    imperialShip.takeDamage(rebelShip.getAttackPower());
+}
+
+template <typename I, typename T> void attack(I& imperialShip, XWing<T>& rebelShip) {
+    rebelShip.takeDamage(imperialShip.getAttackPower());
+    imperialShip.takeDamage(rebelShip.getAttackPower());
+}
 
 #endif //STARWARS_IMPERIALFLEET_H
---
+++ src/starwars_example.cc
@@ -0,0 +1,52 @@
+#include "rebelfleet.h"
+#include "imperialfleet.h"
+//#include "battle.h"
+#include <cassert>
+
+int main() {
+    XWing<float> xwing(100.0f, 300000.0f, 50.0f);
+    Explorer<int> explorer(150, 400000);
+    StarCruiser<unsigned> cruiser(1234, 100000, 11);
+    DeathStar<long> deathStar(10000, 75);
+    TIEFighter<unsigned> fighter(50, 9);
+    ImperialDestroyer<int> destroyer(150, 20);
+
+
+    attack(deathStar, explorer);
+    assert(deathStar.getShield() == 10000);
+    assert(explorer.getShield() == 75);
+
+    attack(fighter, xwing);
+    assert(fighter.getShield() == 0);
+    assert(xwing.getShield() == 91);
+
+    attack(destroyer, cruiser);
+    assert(destroyer.getShield() == 139);
+    assert(cruiser.getShield() == 1214);
+
+//    auto battle = SpaceBattle<short, 1, 23,
+//                              DeathStar<long>,
+//                              Explorer<int>,
+//                              TIEFighter<unsigned>,
+//                              XWing<float>>(deathStar,
+//                                            explorer,
+//                                            fighter,
+//                                            xwing);
+//
+//    assert(battle.countRebelFleet() == 2);
+//    assert(battle.countImperialFleet() == 2);
+//
+//    battle.tick(2);
+//    assert(battle.countRebelFleet() == 2);
+//    assert(battle.countImperialFleet() == 1);
+//
+//    battle.tick(1);
+//    assert(battle.countRebelFleet() == 2);
+//    assert(battle.countImperialFleet() == 1);
+//
+//    battle.tick(4);
+//    assert(battle.countRebelFleet() == 0);
+//    assert(battle.countImperialFleet() == 1);
+//
+//    battle.tick(1); // Wypisuje "IMPERIUM WON\n".
+}
