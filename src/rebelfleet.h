4859e74fff032f7149e408c31fbefdeec6b0ff9d
db394217 <db394217@students.mimuw.edu.pl>
2018-12-03 14:54:18 +0100

    rebelfleet.h - consts

--- src/rebelfleet.h
+++ src/rebelfleet.h
@@ -4,12 +4,11 @@
 #include <cassert>
 #include <type_traits>
 
-namespace {
-    constexpr int minSpeedExplorerAndXwing = 299796;
-    constexpr int maxSpeedExplorerAndXwing = 2997960;
-    constexpr int minSpeedStarCruiser = 99999;
-    constexpr int maxSpeedStarCruiser = 299795;
-}
+
+constexpr int minSpeedExplorerAndXwing = 299796;
+constexpr int maxSpeedExplorerAndXwing = 2997960;
+constexpr int minSpeedStarCruiser = 99999;
+constexpr int maxSpeedStarCruiser = 299795;
 
 template<typename U, int minSpeed, int maxSpeed, bool canAttack>
 class RebelStarship {
@@ -20,42 +19,41 @@ private:
 
 public:
     using valueType = U;
-    
+
     template<bool A = canAttack, typename = typename std::enable_if<A>::type>
-    RebelStarship(U shield, const U speed, const U attackPower) : shield(shield), speed(speed),
+    RebelStarship(U shield, U speed, U attackPower) : shield(shield), speed(speed),
                                                                   attackPower(attackPower) {
         assert(minSpeed <= speed);
         assert(maxSpeed >= speed);
     }
-    
+
     template<bool A = canAttack, typename = typename std::enable_if<!A>::type>
-    RebelStarship(U shield, const U speed) : shield(shield), speed(speed), attackPower(0) {
+    RebelStarship(U shield, U speed) : shield(shield), speed(speed), attackPower(0) {
         assert(minSpeed <= speed);
         assert(maxSpeed >= speed);
     }
-    
+
     U getShield() const {
         return shield;
     }
-    
+
     U getSpeed() const {
         return speed;
     }
-    
+
     void takeDamage(U damage) {
         if (shield > damage) {
             shield -= damage;
-        }
-        else {
+        } else {
             shield = 0;
         }
     }
-    
+
     template<bool A = canAttack, typename = typename std::enable_if<A>::type>
     U getAttackPower() const {
         return attackPower;
     }
-    
+
 };
 
 template<typename U>
